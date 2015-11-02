a326940800ffb7ac0308b2c5b0c2fabaac8643c6
Fletcher Nichol <fnichol@nichol.ca>
2015-11-01 21:15:52 -0700

    Re-enable `scripts/deploy.sh`.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,17 +7,17 @@ info() { echo "    $*"; }
 image="${1:-${IMAGE}}"
 
 banner "Logging in to Docker Hub..."
-echo docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 info "Login success."
 
 banner "Pushing $image"
-echo docker push "$image"
+docker push "$image"
 info "Pushing $image complete."
 
 if [ -n "${LATEST:-}" ]; then
   latest_image="`echo $image | awk -F: '{print $1}'`:latest"
   banner "Pushing $latest_image"
-  echo docker push "$latest_image"
+  docker push "$latest_image"
   info "Pushing $latest_image complete."
 fi
 
